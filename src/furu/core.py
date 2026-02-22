474d1a9c06a2128bd6ae41bd38368afab18569a3
Herman Brunborg <herman@brunborg.com>
2026-02-22 07:58:53 -0800

    starting to refactor into multiple files

--- src/furu/core.py
+++ src/furu/core.py
@@ -1,8 +1,4 @@
 import enum
-import hashlib
-import json
-import types
-import typing
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, fields, is_dataclass
 from functools import cache, cached_property
@@ -10,13 +6,14 @@ from pathlib import Path
 from typing import (
     TYPE_CHECKING,
     Any,
-    get_args,
-    get_origin,
-    get_type_hints,
 )
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import JsonValue
+
+from furu.constants import CLASSMARKER
+from furu.schema import _schema_type
+from furu.utils import _hash_dict_deterministically, fully_qualified_name
 
 if TYPE_CHECKING:
     from typing_extensions import dataclass_transform
@@ -28,11 +25,6 @@ else:
 
     class _FuruDataclassTransform:
         pass
-
-
-CLASSMARKER = "|class"
-ORIGINMARKER = "|origin"
-ARGSMARKER = "|args"
 
 
 class Furu[T](_FuruDataclassTransform, ABC):
@@ -60,96 +52,11 @@ class Furu[T](_FuruDataclassTransform, ABC):
 
     @cached_property
     def furu_schema(self) -> JsonValue:
-        seen = set()
-
-        def _schema_dataclass(tp: type) -> JsonValue:
-            if tp in seen:
-                return {CLASSMARKER: _type_fqn(tp)}
-            seen.add(tp)
-
-            hints = get_type_hints(tp, include_extras=True)
-            return {
-                CLASSMARKER: _type_fqn(tp),
-                "fields": {
-                    f.name: _schema_type(hints.get(f.name, f.type))
-                    for f in sorted(fields(tp), key=lambda f: f.name)
-                    if not f.name.startswith("_")
-                },
-            }
-
-        def _schema_type(tp: Any) -> JsonValue:
-            origin = get_origin(tp)
-
-            if (isinstance(tp, type) and is_dataclass(tp)) or (
-                is_dataclass(origin) and (tp := origin)
-            ):
-                return _schema_dataclass(tp)
-
-            if origin in (typing.Union, types.UnionType):
-                return sorted(
-                    [_schema_type(a) for a in get_args(tp)], key=_stable_json_dump
-                )
-            elif origin is not None:
-                assert (args := get_args(tp))  # TODO: maybe i need to remove this?
-                return {
-                    ORIGINMARKER: _type_fqn(origin),
-                    ARGSMARKER: sorted(
-                        [_schema_type(a) for a in args], key=_stable_json_dump
-                    ),
-                }
-
-            if tp in [str, float, int, bool]:
-                return _type_fqn(tp)
-            elif isinstance(tp, str):
-                return tp
-            elif tp in [list, tuple, dict]:
-                assert get_args(tp) == ()
-                return _type_fqn(tp)
-            elif isinstance(tp, typing.TypeVar):
-                return repr(tp)
-            elif isinstance(tp, enum.EnumType):
-                return _type_fqn(tp)
-            assert False, f"TODO: unexpected type value {tp=}"
-
-        seen = set()
-
-        return _schema_type(type(self))
+        return _schema_type(type(self), set())
 
     @cached_property
     def furu_schema_hash(self) -> str:
         return _hash_dict_deterministically(self.furu_schema)
-
-
-def _stable_json_dump(x: JsonValue) -> str:
-    return json.dumps(x, sort_keys=True, separators=(",", ":"))
-
-
-def _hash_dict_deterministically(obj: JsonValue) -> str:
-    json_str = _stable_json_dump(obj)
-
-    return hashlib.blake2s(
-        json_str.encode(),
-        digest_size=10,  # TODO: make this digest size configurable and include a script for estimating likelihood of crashing. right now, i think there is a 1e-08 chance of a collision with 155M items with the same schema and namespace
-    ).hexdigest()
-
-
-def _type_fqn(tp: type) -> str:
-    mod = tp.__module__
-    qualname = tp.__qualname__
-    if mod == "__main__":  # TODO: allow overwriting
-        raise ValueError("Cannot serialize objects from __main__ module")
-    elif "<locals>" in mod:  # TODO: allow overwriting
-        raise ValueError("TODO: msg")
-    elif "." in qualname:
-        raise ValueError("TODO: msg")
-    elif isinstance(tp, enum.Enum):
-        raise ValueError(
-            "TODO: support this in the future"
-        )  # return f"{mod}.{qualname}.{obj.name}"
-    return f"{mod}.{qualname}"
-
-
-# TODO: should i cache this?
 
 
 @cache
@@ -178,12 +85,12 @@ def to_json(
             return {assert_correct_dict_key(k): to_json(v) for k, v in obj.items()}
         case x if is_dataclass(x):
             return {
-                CLASSMARKER: _type_fqn(type(x)),
+                CLASSMARKER: fully_qualified_name(type(x)),
                 **{f.name: to_json(getattr(x, f.name)) for f in fields(x)},
             }
         case PydanticBaseModel():
             return {
-                CLASSMARKER: _type_fqn(type(obj)),
+                CLASSMARKER: fully_qualified_name(type(obj)),
                 **{k: to_json(v) for k, v in obj.model_dump().items()},
             }
         case enum.Enum():
---
+++ src/furu/schema.py
@@ -0,0 +1,61 @@
+import enum
+import types
+import typing
+from dataclasses import fields, is_dataclass
+from typing import (
+    Any,
+    get_args,
+    get_origin,
+    get_type_hints,
+)
+
+from pydantic import JsonValue
+
+from furu.constants import CLASSMARKER
+
+
+def _schema_dataclass(tp: type, seen: set[type]) -> JsonValue:
+    if tp in seen:
+        return {CLASSMARKER: fully_qualified_name(tp)}
+    seen.add(tp)
+
+    hints = get_type_hints(tp, include_extras=True)
+    return {
+        CLASSMARKER: fully_qualified_name(tp),
+        "fields": {
+            f.name: _schema_type(hints.get(f.name, f.type))
+            for f in sorted(fields(tp), key=lambda f: f.name)
+            if not f.name.startswith("_")
+        },
+    }
+
+
+def _schema_type(tp: Any, seen: set[type]) -> JsonValue:
+    origin = get_origin(tp)
+
+    if (isinstance(tp, type) and is_dataclass(tp)) or (
+        is_dataclass(origin) and (tp := origin)
+    ):
+        return _schema_dataclass(tp)
+
+    if origin in (typing.Union, types.UnionType):
+        return sorted([_schema_type(a) for a in get_args(tp)], key=_stable_json_dump)
+    elif origin is not None:
+        assert (args := get_args(tp))  # TODO: maybe i need to remove this?
+        return {
+            ORIGINMARKER: fully_qualified_name(origin),
+            ARGSMARKER: sorted([_schema_type(a) for a in args], key=_stable_json_dump),
+        }
+
+    if tp in [str, float, int, bool]:
+        return fully_qualified_name(tp)
+    elif isinstance(tp, str):
+        return tp
+    elif tp in [list, tuple, dict]:
+        assert get_args(tp) == ()
+        return fully_qualified_name(tp)
+    elif isinstance(tp, typing.TypeVar):
+        return repr(tp)
+    elif isinstance(tp, enum.EnumType):
+        return fully_qualified_name(tp)
+    assert False, f"TODO: unexpected type value {tp=}"
