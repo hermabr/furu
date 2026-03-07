9331bc6310ca2b7b43363ee736fe9062f422699c
Marcel Rød <marcelroed@gmail.com>
2026-03-06 17:19:15 -0800

    Use multiprocessing, not yet migrated to custom file locking

--- src/furu/locking.py
+++ src/furu/locking.py
@@ -1,15 +1,21 @@
 # TODO: this is the first iteration of this file, but it has multiple errors and footguns, such as the gil being busy for more than LeaseConfig.lifetime_s or the heartbeat thread losing the lock but not notifying/killing the worker. i will either move this to zig or rewrite it at some point
 # TODO: handle cases where a user submits a 8 task torch job
+import multiprocessing as mp
 import os
+from inspect import FrameInfo
+
 import pickle
 import threading
+import time
 from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import datetime
+from multiprocessing import Process
 from pathlib import Path
 from typing import Literal
+import signal
 
-from flufl.lock import Lock, NotLockedError, TimeOutError
+from flufl.lock import AlreadyLockedError, Lock, NotLockedError, TimeOutError
 
 from furu.utils import Ok
 
@@ -51,8 +57,8 @@ def run_with_lease_and_pickle_result[T](
         lease_config = LeaseConfig()
     # TODO: make the directory for the data and the <data-dir>/.furu
     # TODO: if the gil is busy for more than lease_config.lifetime_s we will lose the lock. move to zig or use multiprocessing. the reason we are not already using multiprocessing is that it is more painful to return the result from the self._create call when we use multiprocessing
-    stop_lease_heartbeat = threading.Event()
-    lease_lost = threading.Event()
+    # stop_lease_heartbeat = threading.Event()
+    # lease_lost = threading.Event()
     now = datetime.now()
     staged_result_path = result_path.with_suffix(
         f".{os.getpid()}-{now:%y%m%d_%H-%M-%S}.pkl.tmp"
@@ -62,24 +68,38 @@ def run_with_lease_and_pickle_result[T](
         file_lock = Lock(str(lock_path), lifetime=lease_config.lease_ttl_s)
 
         try:
-            file_lock.lock(timeout=0)  # TODO:make sure timeout 0 makes sense
+            file_lock.lock(timeout=0)
         except TimeOutError as e:
             print(f"failed to lock {file_lock} at path {str(lock_path)}")
             raise NotImplementedError(
                 "TODO: wait for the object to finish and return/read a copy for some time, die if you wait for too long and check if the old lease is expired (should be automatic)"
             ) from e
 
+        if result_path.exists():
+            raise NotImplementedError(
+                "TODO: result exists, but got lock. maybe just read the cached value"
+            )
+        
+    
+
         def review_lease_loop() -> None:
-            while not stop_lease_heartbeat.wait(lease_config.review_interval_s):
+            def lease_loop_handler(signum: signal.Signals, frame: FrameInfo | None) -> None:
+            
+            while True:
                 try:
                     file_lock.refresh()
+                    time.sleep(lease_config.review_interval_s)
                 except NotLockedError:
-                    lease_lost.set()
-                    stop_lease_heartbeat.set()
-                    return  # TODO: How do i fail/throw correctly here?
+                    raise NotImplementedError("TODO: handle this")
+                    # lease_lost.set()
+                    # stop_lease_heartbeat.set()
+                    # return  # TODO: How do i fail/throw correctly here?
 
-        lease_heartbeat_thread = threading.Thread(target=review_lease_loop, daemon=True)
-        lease_heartbeat_thread.start()
+        # lease_heartbeat_thread = threading.Thread(target=review_lease_loop, daemon=True)
+        # lease_heartbeat_thread.start()
+        ctx = mp.get_context("spawn")
+        lease_heartbeat_process = ctx.Process(target=review_lease_loop, daemon=True)
+        lease_heartbeat_process.start()
 
         try:
             result = _compute_and_stage_pickle_result(
@@ -114,4 +134,9 @@ def run_with_lease_and_pickle_result[T](
         if staged_result_path.exists():
             raise NotImplementedError("TODO: i don't think this can ever happen?")
 
-        file_lock.unlock()  # TODO: this can technically fail if we didn't aquire the lock. won't fix this since this logic will be moved to zig
+        try:
+            file_lock.unlock(
+                unconditionally=True
+            )  # TODO: this can technically fail if we didn't aquire the lock. won't fix this since this logic will be moved to zig
+        except NotLockedError:  # Didn't need to unlock
+            pass  # TODO: should we log that we weren't able to lock?
