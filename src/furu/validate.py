0dd0f5b82a38bb87809d3c6ea6a5e50701cc24a0
Herman Brunborg <herman@brunborg.com>
2026-03-22 15:30:02 -0700

    Run validators after post-init hooks
    - Inline validation execution into the generated `__post_init__`
    - Preserve inherited and user-defined post-init order before local validators

--- src/furu/validate.py
+++ src/furu/validate.py
@@ -11,32 +11,26 @@ class validate:
         owner.__furu_local_validators__.append(self.fn)
         setattr(owner, name, self.fn)
 
-
-def run_validation(instance: Any, validators: list[Callable[[Any], None]]) -> None:
-    for validator in validators:
-        validator(instance)
-
-
-def find_inherited_post_init(cls: type) -> Any:
-    for base in cls.__mro__[1:]:
-        post_init = base.__dict__.get("__post_init__")
-        if post_init is not None:
-            return post_init
-    return None
-
-
 def install_validation_hooks(cls: type) -> None:
     user_post_init = cls.__dict__.get("__post_init__")
-    inherited_post_init = find_inherited_post_init(cls)
     local_validators = cls.__dict__.get("__furu_local_validators__", [])
     if user_post_init is None and not local_validators:
         return
+    inherited_post_init = next(
+        (
+            post_init
+            for base in cls.__mro__[1:]
+            if (post_init := base.__dict__.get("__post_init__")) is not None
+        ),
+        None,
+    )
 
     def __post_init__(self, *args: Any, **kwds: Any) -> None:
         if inherited_post_init is not None:
             inherited_post_init(self, *args, **kwds)
         if user_post_init is not None:
             user_post_init(self, *args, **kwds)
-        run_validation(self, local_validators)
+        for validator in local_validators:
+            validator(self)
 
     setattr(cls, "__post_init__", __post_init__)
